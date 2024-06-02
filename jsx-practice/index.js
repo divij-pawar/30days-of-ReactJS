4cef2db8a53a3832d6d1937652c13b1fb17a4966
divij-pawar <divijpawar9@gmail.com>
2024-06-02 19:49:23 -0400

    jsx

---
+++ jsx-practice/index.js
@@ -0,0 +1,15 @@
+
+const navbar = (
+    <nav>
+    <h1>Website</h1>
+    <ul>
+    <li>Pricing</li>
+    <li>About</li>
+    <li>Contact</li>
+    </ul>
+    </nav>
+)
+ReactDOM.render(
+    navbar,
+    document.getElementById("root")
+)
